9ef1c55b942e94dcad90c6bb5162a30f6126b984
MarcosGG86 <101665058+MarcosGG86@users.noreply.github.com>
2022-03-16 01:03:01 +0100

    commit JavaDoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -2,86 +2,138 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+/**
+ * paquete al que pertenece la clase CCuenta
+ */
 package cuentas;
 
- 
+/**
+ * Clase CCuenta que posee los metodos para crear una cuenta, getters y setters
+ * de sus atributos q ingresar y retirar dinero
+ *
+ * @author Marcos
+ */
 public class CCuenta {
 
-
+    /**
+     * Atributos de CCuenta
+     */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-
-    public CCuenta()
-    {
+    /**
+     * Constructor vacio
+     */
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    /**
+     * Constructor
+     *
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public void asignarNombre(String nom)
-    {
+    /**
+     *
+     * @param nom the nom to set
+     */
+    public void asignarNombre(String nom) {
         setNombre(nom);
     }
 
-    public String obtenerNombre()
-    {
+    /**
+     *
+     * @return getNombre
+     */
+    public String obtenerNombre() {
         return getNombre();
     }
 
-
-    public double estado()
-    {
+    /**
+     *
+     * @return getSaldo
+     */
+    public double estado() {
         return getSaldo();
     }
 
-
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * Metodo para comprobar si se puede ingresar saldo en caso negativo lanza
+     * una excepcion
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    /**
+     * Metodo para comprobar si se puede retirar saldo en caso negativo lanza
+     * una excepcion
+     *
+     * @param cantidad
+     * @throws Exception
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
 
-
-    public String obtenerCuenta()
-    {
+    /**
+     *
+     * @return getCuenta
+     */
+    public String obtenerCuenta() {
         return getCuenta();
     }
 
-
+    /**
+     *
+     * @return nombre
+     */
     public String getNombre() {
         return nombre;
     }
 
-
+    /**
+     *
+     * @param nombre the nombre to set
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-
+    /**
+     *
+     * @return tipoInteres
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
+    /**
+     *
+     * @param tipoInterés the tipoInterés to set
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -3,21 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+/**
+ * Paquete al que pertenece la clase main
+ */
 package cuentas;
 
-
+/**
+ * Clase que contiene el metodo main y realiza operaciones con el objeto cuenta1
+ *
+ * @author Marcos
+ */
 public class Main {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
 
         cuenta1 = operativa_cuenta(2500);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
-
+        System.out.println("El saldo actual es" + saldoActual);
+        /**
+         * Comprobacion mediante Excepciones de si se puede retirar o ingresar
+         * el saldo
+         */
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
@@ -31,9 +43,15 @@ public class Main {
         }
     }
 
+    /**
+     * Metodo para asignar por parametro una cantidad al objeto cuenta1
+     *
+     * @param cantidad
+     * @return cuenta1
+     */
     public static CCuenta operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         return cuenta1;
     }
 }
